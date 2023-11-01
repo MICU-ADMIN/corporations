e02565d173de57fdc567cc857c8898a603b6b5e8
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-11-01 13:48:34 +0000

    swagger

---
+++ api/WeatherForecast.cs
@@ -0,0 +1,20 @@
+﻿// Define a class named "WeatherForecast" within the "api" namespace.
+namespace api
+{
+    // This class represents weather forecast data.
+
+    public class WeatherForecast
+    {
+        // Property to store the date of the weather forecast.
+        public DateOnly Date { get; set; }
+
+        // Property to store temperature in degrees Celsius.
+        public int TemperatureC { get; set; }
+
+        // Property to calculate temperature in degrees Fahrenheit based on TemperatureC.
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        // Property to store a summary of the weather forecast (nullable string).
+        public string? Summary { get; set; }
+    }
+}
