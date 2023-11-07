5ce906b578b00573af0249a7d88c1b532de018be
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-11-07 02:47:31 +0000

    rebuild

--- api/app.go
+++ api/app.go
@@ -1,32 +1,154 @@
 package main
 
 import (
-	"embed"
-	"html/template"
-    	"net/http"
+	"database/sql"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-//go:embed templates/*
-var templates embed.FS
-var t = template.Must(template.ParseFS(templates, "templates/*"))
+var db *sql.DB
 
-//go:embed static/*
-var assets embed.FS
-
+type Hadith struct {
+	Id    int64
+	Name  string
+	Price int
+}
 
 func main() {
-	port := "3000"
-	if(os.Getenv("PORT") != "") {
-		port = os.Getenv("PORT")
+
+err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env", err)
 	}
 
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        t.Execute(w, "")
-    })
+	// Open a connection to the database
+	db, err = sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatal("failed to open db connection", err)
+	}
 
-    fs := http.FileServer(http.FS(assets))
-    http.Handle("/static/", fs)
 
-    http.ListenAndServe(":" + port, nil)
+	router := gin.Default()
+
+router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello")
+	})
+
+// hadiths
+	router.GET("/hadiths", GetHadiths)
+
+
+	
+	router.Run()
+}
+
+
+// services 
+
+
+func GetHadiths(c *gin.Context) {
+	query := "SELECT * FROM Hadith"
+	res, err := db.Query(query)
+	defer res.Close()
+	if err != nil {
+		log.Fatal("(GetHadiths) db.Query", err)
+	}
+
+	hadiths := []Hadith{}
+	for res.Next() {
+		var hadith Hadith
+		err := res.Scan(&hadith.Id, &hadith.Name, &hadith.Price)
+		if err != nil {
+			log.Fatal("(GetHadiths) res.Scan", err)
+		}
+		hadiths = append(hadiths, hadith)
+	}
+
+	c.JSON(http.StatusOK, hadiths)
+}
+
+func GetSingleHadith(c *gin.Context) {
+	hadithId := c.Param("hadithId")
+	hadithId = strings.ReplaceAll(hadithId, "/", "")
+	hadithIdInt, err := strconv.Atoi(hadithId)
+	if err != nil {
+		log.Fatal("(GetSingleHadith) strconv.Atoi", err)
+	}
+
+	var hadith Hadith
+	query := `SELECT * FROM hadiths WHERE id = ?`
+	err = db.QueryRow(query, hadithIdInt).Scan(&hadith.Id, &hadith.Name, &hadith.Price)
+	if err != nil {
+		log.Fatal("(GetSingleHadith) db.Exec", err)
+	}
+
+	c.JSON(http.StatusOK, hadith)
+}
+
+func CreateHadith(c *gin.Context) {
+	var newHadith Hadith
+	err := c.BindJSON(&newHadith)
+	if err != nil {
+		log.Fatal("(CreateHadith) c.BindJSON", err)
+	}
+
+	query := `INSERT INTO Hadith (name, price) VALUES (?, ?)`
+	res, err := db.Exec(query, newHadith.Name, newHadith.Price)
+	if err != nil {
+		log.Fatal("(CreateHadith) db.Exec", err)
+	}
+	newHadith.Id, err = res.LastInsertId()
+	if err != nil {
+		log.Fatal("(CreateHadith) res.LastInsertId", err)
+	}
+
+	c.JSON(http.StatusOK, newHadith)
+}
+
+func UpdateHadith(c *gin.Context) {
+	var updates Hadith
+	err := c.BindJSON(&updates)
+	if err != nil {
+		log.Fatal("(UpdateHadith) c.BindJSON", err)
+	}
+
+	hadithId := c.Param("hadithId")
+	hadithId = strings.ReplaceAll(hadithId, "/", "")
+	hadithIdInt, err := strconv.Atoi(hadithId)
+	if err != nil {
+		log.Fatal("(UpdateHadith) strconv.Atoi", err)
+	}
+
+	query := `UPDATE Hadith SET name = ?, price = ? WHERE id = ?`
+	_, err = db.Exec(query, updates.Name, updates.Price, hadithIdInt)
+	if err != nil {
+		log.Fatal("(UpdateHadith) db.Exec", err)
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func DeleteHadith(c *gin.Context) {
+	hadithId := c.Param("hadithId")
+
+	hadithId = strings.ReplaceAll(hadithId, "/", "")
+	hadithIdInt, err := strconv.Atoi(hadithId)
+	if err != nil {
+		log.Fatal("(DeleteHadith) strconv.Atoi", err)
+	}
+	query := `DELETE FROM Hadith WHERE id = ?`
+	_, err = db.Exec(query, hadithIdInt)
+	if err != nil {
+		log.Fatal("(DeleteHadith) db.Exec", err)
+	}
+
+	c.Status(http.StatusOK)
 }
